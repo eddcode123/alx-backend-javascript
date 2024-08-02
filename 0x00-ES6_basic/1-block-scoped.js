5dde280b937200b0dcc9b41583c74f0c70587e85
eddcode123 <edwinmutugi29@gmail.com>
2024-08-02 13:05:28 +0300

    using let in if block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    // eslint-disable-next-line no-empty
-    if (trueOrFalse) {
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
   }
+
+  return [task, task2];
+}
