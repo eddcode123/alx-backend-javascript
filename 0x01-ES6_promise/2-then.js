dc844bb8e7abce0155c3086ae76b10d3806f41a2
eddcode123 <edwinmutugi29@gmail.com>
2024-11-11 10:17:56 +0300

    using and channing thenables in a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 function handleResponseFromAPI(promise) {
-  return promise.then(() => ({status: 200, body: 'success'}).catch(() => new Error()).finally(() => {console.log("Got a response from the API")}));
+  return promise.then(() => ({status: 200, body: 'success'})).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
 }
