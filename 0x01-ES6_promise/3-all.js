fce79ce0a25a7efd85265c6d2411b6660c294e4a
eddcode123 <edwinmutugi29@gmail.com>
2024-11-11 10:51:28 +0300

    resolving mutiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser]).then((result) => { console.log(result.FirstNmae, result.lastName); }).catch(() => { console.error('Signup system offline'); });
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([upR, userR]) => { console.log(upR.body, userR.firstName, userR.lastName); })
+    .catch(() => { console.error('Signup system offline'); });
 }
