9c24646f3775ed26112ccfb18733435ddac30400
eddcode123 <edwinmutugi29@gmail.com>
2024-11-11 11:35:40 +0300

    returning a resolved promise object

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,3 @@
-/* eslint-disable import/extensions */
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({
-      firstName,
-      lastName,
-    });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
