aa0338022c37a354628acc702dfcc8c8fcbdef65
eddcode123 <edwinmutugi29@gmail.com>
2024-11-15 11:48:29 +0300

    implementing a method in the car class that returns a new object of the class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,10 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw new Error();
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  //  method returns a new object of the class
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
   }
