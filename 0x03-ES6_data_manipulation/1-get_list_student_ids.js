8f558b753b2ef9f1606cb787348e68c94b97a26b
eddcode123 <edwinmutugi29@gmail.com>
2024-11-18 12:31:32 +0300

    delete files

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line consistent-return
-export default function getListStudentIds(arg) {
-  if (!Array.isArray(arg)) return [];
-  return arg.map((el) => el.id);
-}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++
@@ -1,6 +0,0 @@
-export default function hasValuesFromArray(set, arr) {
-  for (const el of arr) {
-    if (!set.has(el)) return false;
-  }
-  return true;
-}
